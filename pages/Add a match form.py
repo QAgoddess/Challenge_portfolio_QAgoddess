6c5a71d402ed9d9c97810007fce43091163b63bf
QAgoddess <137486413+QAgoddess@users.noreply.github.com>
2023-07-05 21:41:37 +0200

    selectors tasks

---
+++ pages/Add a match form.py
@@ -0,0 +1,17 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    header_xpath = "//*[@id="__next"]/div[1]/header"
+    header_text_xpath = "//*[contains(@class, "jss16")]"
+    main_page_xpath = "//div[contains(@class, "443")]"
+    players_xpath = "//div[contains(@class, "444")]"
+    test_test_xpath = "//div[contains(@class, "445")]"
+    matches_xpath = "//span[text()='Matches']"
+    reports_xpath = "//span[text()='Reports']"
+    polski_xpath = "//span[text()='Polski']"
+    sign_out_xpath = "//*[text()='Sign out']"
+    add_match_xpath = "//a[contains(@class, "0")]"
+    table_xpath = "//table"
+    thead_xpath = "//thead"
+    tbody_xpath = "//tbody"
